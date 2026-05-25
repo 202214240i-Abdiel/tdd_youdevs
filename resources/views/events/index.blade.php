56693021ba27d96e9245a6399212a5d73aa4516d
202214240i-Abdiel <202214240i@utea.edu.pe>
2026-05-25 16:05:38 -0500

    Crear proyecto Laravel base

--- resources/views/events/index.blade.php
+++
@@ -1,8 +0,0 @@
-@foreach ($events as $event)
-    <div class="event">
-        <h2>{{ $event->name }}</h2>
-        <p>Fecha: {{ $event->date }}</p>
-        <p>Hora: {{ $event->time }}</p>
-        <p>Ubicación: {{ $event->location }}</p>
-    </div>
-@endforeach
